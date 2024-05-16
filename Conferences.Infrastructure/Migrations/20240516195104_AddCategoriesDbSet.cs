79397236b308e8d45a1384ce735246a75168715f
mateusz5564 <mateusz5564@gmail.com>
2024-05-16 21:53:19 +0200

    AddCategoriesDbSet migration.

---
+++ Conferences.Infrastructure/Migrations/20240516195104_AddCategoriesDbSet.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conferences.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriesDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conferences_Category_CategoryId",
+                table: "Conferences");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conferences_Categories_CategoryId",
+                table: "Conferences",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conferences_Categories_CategoryId",
+                table: "Conferences");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conferences_Category_CategoryId",
+                table: "Conferences",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
