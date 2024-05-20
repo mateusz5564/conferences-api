ae36a0020df8a07cd3fb98f734445d8c169c8110
mateusz5564 <mateusz5564@gmail.com>
2024-05-20 12:24:15 +0200

    Fix LocationDto types.

--- Conferences.Application/Conferences/Dtos/LocationDto.cs
+++ Conferences.Application/Conferences/Dtos/LocationDto.cs
@@ -3,7 +3,7 @@ namespace Conferences.Application.Conferences.Dtos
 {
     public class LocationDto
     {
-        public string Latitude { get; set; } = default!;
-        public string Longitude { get; set; } = default!;
+        public double Latitude { get; set; } = default!;
+        public double Longitude { get; set; } = default!;
     }
 }
