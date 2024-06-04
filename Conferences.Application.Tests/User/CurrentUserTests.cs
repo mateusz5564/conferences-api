b0492b59ec4f24dff62095f6db43bd84a8a5c030
mateusz5564 <mateusz5564@gmail.com>
2024-06-04 22:18:54 +0200

    Unit test UserContext

---
+++ Conferences.Application.Tests/User/CurrentUserTests.cs
@@ -0,0 +1,29 @@
+﻿using Conferences.Domain.Constants;
+using FluentAssertions;
+using Xunit;
+
+namespace Conferences.Application.User.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            var user = new CurrentUser("1", "test@interia.pl", [UserRoles.Admin]);
+
+            var isInRole = user.IsInRole(UserRoles.Admin);
+
+            isInRole.Should().BeTrue();
+        }  
+        
+        [Fact()]
+        public void IsInRole_WithNotMatchingRole_ShouldReturnFalse()
+        {
+            var user = new CurrentUser("1", "test@interia.pl", []);
+
+            var isInRole = user.IsInRole(UserRoles.Admin);
+
+            isInRole.Should().BeFalse();
+        }
+    }
+}
---
+++ Conferences.Application.Tests/User/UserContextTests.cs
@@ -0,0 +1,57 @@
+﻿using Conferences.Domain.Constants;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using Xunit;
+
+namespace Conferences.Application.User.Tests
+{
+    public class UserContextTests
+    {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.NameIdentifier, "1"),
+                new (ClaimTypes.Email, "test@interia.pl"),
+                new (ClaimTypes.Role, UserRoles.Admin)
+
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            var currentUser = userContext.GetCurrentUser();
+
+            currentUser.Should().NotBeNull();
+            currentUser.Id.Should().Be("1");
+            currentUser.Email.Should().Be("test@interia.pl");
+            currentUser.Roles.Should().Contain(UserRoles.Admin);
+        }
+
+        [Fact()]
+        public void GetCurrentUser_WithoutAuthenticatedUser_ShouldThrowException()
+        {
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns((HttpContext)null);
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            Action action = () => userContext.GetCurrentUser();
+
+            action
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("User context is not present");
+        }
+    }
+}
