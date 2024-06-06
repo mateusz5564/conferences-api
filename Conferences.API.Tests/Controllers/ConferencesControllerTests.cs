22af3fec88ba9365427a854c19553fc0d62a8f9e
mateusz5564 <mateusz5564@gmail.com>
2024-06-06 17:38:57 +0200

    Controller integration tests for GetAll endpoint

---
+++ Conferences.API.Tests/Controllers/ConferencesControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace Conferences.API.Controllers.Tests
+{
+    public class ConferencesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ConferencesControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact()]
+        public async Task GetAll_ForValidRequest_Returns200Ok()
+        {
+            var client = _factory.CreateClient();
+
+            var result = await client.GetAsync("api/conferences?pageNumber=1&pageSize=5");
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        } 
+        
+        [Fact()]
+        public async Task GetAll_ForInvalidRequest_Returns400BadRequest()
+        {
+            var client = _factory.CreateClient();
+
+            var result = await client.GetAsync("api/conferences");
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
