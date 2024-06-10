8e331454967699d81bddfb17084c9a156c4bcc91
mateusz5564 <mateusz5564@gmail.com>
2024-06-10 13:46:59 +0200

    Add try-catch block to catch startup exceptions

--- src/Conferences.API/Program.cs
+++ src/Conferences.API/Program.cs
@@ -7,46 +7,56 @@ using Conferences.Domain.Entities;
 using Conferences.API.Extensions;
 using Conferences.Infrastructure.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.AddPresentation();
-
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddApplication();
-
-var app = builder.Build();
-
-var scope = app.Services.CreateScope();
-
-var databaseMigrator = scope.ServiceProvider.GetRequiredService<IDatabaseMigrator>();
-await databaseMigrator.ApplyMigrations();
-
-var categorySeeder = scope.ServiceProvider.GetRequiredService<ICategorySeeder>();
-var userRoleSeeder = scope.ServiceProvider.GetRequiredService<IUserRoleSeeder>();
-await categorySeeder.Seed();
-await userRoleSeeder.Seed();
-
-app.UseMiddleware<ErrorHandlingMiddleware>();
-
-app.UseMiddleware<RequestTimeLoggingMiddleware>();
-
-app.UseSerilogRequestLogging();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.AddPresentation();
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddApplication();
+
+    var app = builder.Build();
+
+    var scope = app.Services.CreateScope();
+
+    var databaseMigrator = scope.ServiceProvider.GetRequiredService<IDatabaseMigrator>();
+    await databaseMigrator.ApplyMigrations();
+
+    var categorySeeder = scope.ServiceProvider.GetRequiredService<ICategorySeeder>();
+    var userRoleSeeder = scope.ServiceProvider.GetRequiredService<IUserRoleSeeder>();
+    await categorySeeder.Seed();
+    await userRoleSeeder.Seed();
+
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+
+    app.UseMiddleware<RequestTimeLoggingMiddleware>();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapGroup("api/identity").MapIdentityApi<User>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapGroup("api/identity").MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application startup failed");
+} finally
+{
+    Log.CloseAndFlush();
+}
 
 public partial class Program { }
